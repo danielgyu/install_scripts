fdc56523703e1ff294652e885e12eb0e40f2df0c
kungyu <hismayfly@gmail.com>
2021-12-15 23:59:41 +0900

    goCD script

---
+++ install_gocd.sh
@@ -0,0 +1,5 @@
+echo "deb https://download.gocd.org /" | sudo tee /etc/apt/sources.list.d/gocd.list
+curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo apt-key add -
+sudo apt-get update
+
+sudo apt-get install go-server
